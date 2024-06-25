5dba1c5cfc85181c50be07e933c1317ec2ef845a
Svanidze12 <androsvanidze2008@gmail.com>
2024-06-25 12:56:01 +0400

    commit

---
+++ Day 44/Homework/index.js
@@ -0,0 +1,17 @@
+function numbers(){
+    let num1 = Number(prompt("enter your number here"));
+    let num2 = Number(prompt("enter your number here"));
+    let num3 = Number(prompt("enter your number here"));
+    
+    console.log(num1 * num2 + num3)
+    console.log(num1 + num2 + num3);
+    alert("equel =",)
+}
+const p = document.getElementById("paragraph")
+const buttons = document.getElementById("button");
+let wor
+function change(){
+    buttons.addEventListener("click",function(){
+        p.
+    })
+}
